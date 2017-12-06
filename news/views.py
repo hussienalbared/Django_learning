cd058f8b82765e806c66f95f6f3d16d017a77f28
hussienalbared <hussienalbared@gmail.com>
2017-12-06 23:21:01 +0200

    add grid style small and large

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import news
 # Create your views here.
 def index(request):
     allnews=news.objects.all()
-    return render(request,'indexnews.html',{'news':allnews})
+    agnecies=news.objects.values_list('Agency',flat=True).distinct()
+    return render(request,'indexnews.html',{'news':allnews,'agencies':agnecies})
 def type(request,type):
     allnews=news.objects.filter(category=type)
     return render(request,'indexnews.html',{'news':allnews})
+
+
